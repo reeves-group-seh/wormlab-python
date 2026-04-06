2c1170d7e7fe289d1a30e22a77b71d339a5f917f
Mack Rusing <mail@mackk.net>
2026-04-06 17:32:52 -0400

    feat/errors: remove unused errors module

--- src/stage_ui/errors.py
+++
@@ -1,8 +0,0 @@
-class InvalidStateError(Exception):
-    """
-    Raised when an invalid state is reached. This indicates a logic bug and
-    means the program has reached a state that should not be possible. The
-    program should most likely crash if this error is seen.
-    """
-
-    pass
