919944f2bc9eb7cb2908432ce321e2cf3a909f9d
Mack Rusing <mail@mackk.net>
2026-04-06 17:33:47 -0400

    feat/types: add module to collect types
    Add types to represent:
    
    - `RadiusColor`: color-coded radii corresponding to the marker circles.
    - `FilterNumber`: possible filter numbers.
    - `WormResponse`: possible worm responses.
    - `KeyMapAction`: actions that can be mapped to keys.

---
+++ src/stage_ui/app_types.py
@@ -0,0 +1,62 @@
+# item imports
+from enum import Enum, StrEnum, auto
+
+
+class RadiusColor(StrEnum):
+    """
+    Possible color-coded radii corresponding to the marker circles.
+    """
+
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+    YELLOW = "yellow"
+
+
+class FilterNumber(StrEnum):
+    """
+    Possible filter numbers.
+    """
+
+    ONE = "1"
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
+    SIX = "6"
+
+
+class WormResponse(StrEnum):
+    """
+    Enum representing the possible worm responses.
+    """
+
+    FULL = "F"
+    PARTIAL = "P"
+    ACKNOWLEDGE = "A"
+    NO_RESPONSE = "N"
+
+
+class KeyMapAction(Enum):
+    """
+    Actions mapped to keys.
+    """
+
+    STEP_LEFT = auto()
+    STEP_RIGHT = auto()
+    STEP_UP = auto()
+    STEP_DOWN = auto()
+
+    MOVE_LEFT = auto()
+    MOVE_RIGHT = auto()
+    MOVE_UP = auto()
+    MOVE_DOWN = auto()
+
+    INC_SPEED = auto()
+    DEC_SPEED = auto()
+
+    QUIT = auto()
+    FIRE = auto()
+    DESTROY = auto()
+    GRID = auto()
+    SKIP_DATA = auto()
