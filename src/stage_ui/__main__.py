bb5b5ff6636d56a5a77ed92e8027253d6bc1159c
Mack Rusing <mail@mackk.net>
2026-07-04 12:18:12 -0400

    refactor/main: update argument parsing for better type safety

--- src/stage_ui/__main__.py
+++ src/stage_ui/__main__.py
@@ -1,8 +1,32 @@
 # std
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from enum import StrEnum
 from importlib.resources import files
 from pathlib import Path
-from typing import Any, Literal
+from typing import Any
+
+
+class Args(Namespace):
+    data_dir: Path | None
+    camera_index: int | None
+    arduino_backend: ArduinoBackendArg
+    camera_backend: CameraBackendArg
+    data_backend: DataBackendArg
+
+
+class ArduinoBackendArg(StrEnum):
+    PYSERIAL = "pyserial"
+    MOCK = "mock"
+
+
+class CameraBackendArg(StrEnum):
+    CV2 = "cv2"
+    MOCK = "mock"
+
+
+class DataBackendArg(StrEnum):
+    PANDAS = "pandas"
+    MOCK = "mock"
 
 
 def main() -> None:
@@ -27,71 +51,92 @@ def main() -> None:
     )
     parser.add_argument(
         "--arduino-backend",
-        default="pyserial",
-        choices=["pyserial", "mock"],
+        type=ArduinoBackendArg,
+        default=ArduinoBackendArg.PYSERIAL,
+        choices=list(ArduinoBackendArg),
         help="backend for serial communication with the arduino",
         dest="arduino_backend",
     )
     parser.add_argument(
         "--camera-backend",
-        default="cv2",
-        choices=["cv2", "mock"],
+        type=CameraBackendArg,
+        default=CameraBackendArg.CV2,
+        choices=list(CameraBackendArg),
         help="backend for the camera",
         dest="camera_backend",
     )
     parser.add_argument(
         "--data-backend",
-        default="pandas",
-        choices=["pandas", "mock"],
+        type=DataBackendArg,
+        default=DataBackendArg.PANDAS,
+        choices=list(DataBackendArg),
         help="backend for writing data",
         dest="data_backend",
     )
 
     # parse arguments
-    args = parser.parse_args()
-    data_dir: Path | None = args.data_dir
-    camera_index: int | None = args.camera_index
-    arduino_backend: Literal["pyserial", "mock"] = args.arduino_backend
-    camera_backend: Literal["cv2", "mock"] = args.camera_backend
-    data_backend: Literal["pandas", "mock"] = args.data_backend
-
-    # build up configs's kwargs
-    cfg_kwargs: dict[str, Any] = {}
-    if data_dir is not None:
-        cfg_kwargs["DATA_DIR"] = data_dir
-    if camera_index is not None:
-        cfg_kwargs["DEFAULT_CAMERA_INDEX"] = camera_index
+    args: Args = parser.parse_args(namespace=Args())
 
     # local imports after reading arguments
     import stage_ui
     from stage_ui.app import App
     from stage_ui.app_config import AppConfig
-    from stage_ui.backend_arduino import MockArduinoBackend, PySerialArduinoBackend
-    from stage_ui.backend_camera import CV2CameraBackend, MockCameraBackend
-    from stage_ui.backend_data import MockDataBackend, PandasDataBackend
+    from stage_ui.backend_arduino import (
+        ArduinoBackend,
+        MockArduinoBackend,
+        PySerialArduinoBackend,
+    )
+    from stage_ui.backend_camera import (
+        CameraBackend,
+        CV2CameraBackend,
+        MockCameraBackend,
+    )
+    from stage_ui.backend_data import DataBackend, MockDataBackend, PandasDataBackend
 
     # print startup info
     ascii_art = (files("stage_ui.resources") / "title.txt").read_text()
     print(f"{ascii_art}\nStageUI: starting v{stage_ui.VERSION}")
 
     # create config
+    cfg_kwargs: dict[str, Any] = {}
+    if args.data_dir is not None:
+        cfg_kwargs["DATA_DIR"] = args.data_dir
+    if args.camera_index is not None:
+        cfg_kwargs["DEFAULT_CAMERA_INDEX"] = args.camera_index
     cfg = AppConfig(**cfg_kwargs)
 
-    # create & run app
-    arduino = (
-        MockArduinoBackend()
-        if arduino_backend == "mock"
-        else PySerialArduinoBackend(
-            baudrate=cfg.SERIAL_BAUDRATE,
-            timeout=cfg.SERIAL_TIMEOUT,
-            sleep_factor=cfg.SERIAL_SLEEP_FACTOR,
-        )
-    )
-    camera = MockCameraBackend() if camera_backend == "mock" else CV2CameraBackend()
-    data = MockDataBackend() if data_backend == "mock" else PandasDataBackend()
+    # create arduino backend
+    arduino: ArduinoBackend
+    match args.arduino_backend:
+        case ArduinoBackendArg.PYSERIAL:
+            arduino = PySerialArduinoBackend(
+                baudrate=cfg.SERIAL_BAUDRATE,
+                timeout=cfg.SERIAL_TIMEOUT,
+                sleep_factor=cfg.SERIAL_SLEEP_FACTOR,
+            )
+        case ArduinoBackendArg.MOCK:
+            arduino = MockArduinoBackend()
+
+    # create camera backend
+    camera: CameraBackend
+    match args.camera_backend:
+        case CameraBackendArg.CV2:
+            camera = CV2CameraBackend()
+        case CameraBackendArg.MOCK:
+            camera = MockCameraBackend()
+
+    # create data backend
+    data: DataBackend
+    match args.data_backend:
+        case DataBackendArg.PANDAS:
+            data = PandasDataBackend()
+        case DataBackendArg.MOCK:
+            data = MockDataBackend()
     data.open(cfg.DATA_FILE)
+
+    # create & run app
     app = App(
-        cfg,
+        cfg=cfg,
         arduino=arduino,
         camera=camera,
         data=data,
